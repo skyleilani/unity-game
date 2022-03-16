071894fdee08d6b8cbb9b72184158b331e1eda93
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-03-15 18:26:56 -0700

    Update Explorer.cs
    q and e buttons trigger rotation

--- Scripts/Explorer.cs
+++ Scripts/Explorer.cs
@@ -12,40 +12,45 @@ public class Explorer : MonoBehaviour
     public Vector2 pos;
     private Vector2 smoothPos;
 
-    public float scale;
-    private float smoothScale; 
+    public float scale, angle;
+    private float smoothScale, smoothAngle; 
 
     // to handle user WASD input 
     private void UserInput()
     {
+        if (Input.GetKey("e"))
+            //reduce scale by 1% each time
+            angle += .01f;
+
+        if (Input.GetKey("q"))
+            //reduce scale by 1% each time
+            angle -= .01f;
+
         if (Input.GetKey("w"))
-        {
             //reduce scale by 1% each time
             scale *= .99f;
-        }
 
         if (Input.GetKey("a"))
-        {
             pos.x -= .001f * scale;
-        }
 
         if (Input.GetKey("s"))
-        {
             //increase scale by 1% each time
             scale *= 1.01f;
-        }
-       
+             
         if (Input.GetKey("d"))
-        {
             pos.x += .001f * scale;
-        }
+        
     }
     private void UpdateShader()
     {
-        // position will interpolate between 
-        smoothPos = Vector2.Lerp(smoothPos, pos, 0.03f);
 
+        // Lerp( start_val - when t=0, end_val - when t=1, t - interpolation value) ) 
+        // RETURNS start_val + (end_val - start_val) * t 
+        // position will interpolate between #s
+        smoothPos = Vector2.Lerp(smoothPos, pos, 0.03f);
         smoothScale = Mathf.Lerp(smoothScale, scale, 0.03f);
+        smoothAngle = Mathf.Lerp(smoothAngle, angle, 0.03f);
+
         // fix resizing of fractal to aspect ratio of the screen
         float aspect = (float)Screen.width / (float)Screen.height;
         float scaleX = smoothScale;
@@ -53,7 +58,6 @@ public class Explorer : MonoBehaviour
 
         // if the aspect is larger than 1 then Screen width must be a larger # than Screen height  
         if (aspect > 1f)
-
             scaleY /= aspect;
 
         else
@@ -61,6 +65,9 @@ public class Explorer : MonoBehaviour
         // sets _Area vector4(x, y , z, w ) 
         // pos.x - x component of Vector2 ; pos.y - y component of Vector2 
         mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY)); // setting _Area vector4 (x, y, z, w) 
+       
+        // sets the interp_Angle float 
+        mat.SetFloat("_Angle", smoothAngle);
     }
 
     void FixedUpdate()
