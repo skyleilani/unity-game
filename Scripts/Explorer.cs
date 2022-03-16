d14458c4799fefe36fa5e65487c1ee50f23af786
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-03-15 17:40:40 -0700

    Update Explorer.cs

--- Scripts/Explorer.cs
+++ Scripts/Explorer.cs
@@ -1,3 +1,5 @@
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +8,64 @@ public class Explorer : MonoBehaviour
 {
 
     public Material mat;
+
     public Vector2 pos;
-    public float scale; 
+    private Vector2 smoothPos;
 
-    // Update is called once per frame
-    void Update()
+    public float scale;
+    private float smoothScale; 
+
+    // to handle user WASD input 
+    private void UserInput()
     {
+        if (Input.GetKey("w"))
+        {
+            //reduce scale by 1% each time
+            scale *= .99f;
+        }
 
-       // fix resizing of fractal to aspect ratio of the screen
-       float aspect 
+        if (Input.GetKey("a"))
+        {
+            pos.x -= .001f * scale;
+        }
 
-        // set _Area vector4(x, y , z, w ) 
+        if (Input.GetKey("s"))
+        {
+            //increase scale by 1% each time
+            scale *= 1.01f;
+        }
+       
+        if (Input.GetKey("d"))
+        {
+            pos.x += .001f * scale;
+        }
+    }
+    private void UpdateShader()
+    {
+        // position will interpolate between 
+        smoothPos = Vector2.Lerp(smoothPos, pos, 0.03f);
+
+        smoothScale = Mathf.Lerp(smoothScale, scale, 0.03f);
+        // fix resizing of fractal to aspect ratio of the screen
+        float aspect = (float)Screen.width / (float)Screen.height;
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+
+        // if the aspect is larger than 1 then Screen width must be a larger # than Screen height  
+        if (aspect > 1f)
+
+            scaleY /= aspect;
+
+        else
+            scaleX *= aspect;
+        // sets _Area vector4(x, y , z, w ) 
         // pos.x - x component of Vector2 ; pos.y - y component of Vector2 
-        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scale, scale)); // sets _Area vector4 (x, y, z, w) 
+        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY)); // setting _Area vector4 (x, y, z, w) 
+    }
+
+    void FixedUpdate()
+    {
+        UserInput(); 
+        UpdateShader();
     }
 }
