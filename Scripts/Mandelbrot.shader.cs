815bb3bf384a40199fe5889869e914eb00b931ca
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-03-15 18:11:07 -0700

    Update Mandelbrot.shader.cs

--- Scripts/Mandelbrot.shader.cs
+++ Scripts/Mandelbrot.shader.cs
@@ -8,6 +8,9 @@ Shader "Explorer/Mandelbrot"
         _MainTex ("Texture", 2D) = "white" {}
         // holds area we will render (center, center, size, size) 
         _Area("Area", vector) = (0, 0, 4 , 4)
+
+            // range of -pi to pi in radians
+            _Angle("Angle", range(-3.1415, 3.1415)) = 0
     }
     SubShader
     {
@@ -44,13 +47,13 @@ Shader "Explorer/Mandelbrot"
 
             // we will build our start value based on this area 
             float4 _Area; 
+            float _Angle;
             
             sampler2D _MainTex;
 
             // function rotate point in 2d space 
             // og_p - original point
-            // piv_p -pivot point around which to rotate
-            // a - angle
+            // piv_p -pivot point around w      // a - angle
             float2 rotate(float2 og_p, float piv_p, float a) {
 
                 float s = sin(a);
@@ -64,20 +67,22 @@ Shader "Explorer/Mandelbrot"
                 // new y = original point's X * sin(angle) + original point's Y * cosine(angle) 
                 og_p = float2(og_p.x*c - og_p.y*s, og_p.x*s + og_p.y*c);
 
-                
+                // shift back to original pivot after rotation is done
+                og_p += piv_p; 
+
+                return og_p;
             }
+
             fixed4 frag(v2f i) : SV_Target
             {
-
-                // mandelbrot fractal algorithm 
-
                 // start with start position, initialize to uv coordinate. 
                 float2 start = _Area.xy + (i.uv- 0.5) * _Area.zw; // .zw = last two coords (x, y, z, w) from _Area (4, 4) 
-
+                start = rotate(start, _Area.xy, _Angle);
                 // keep track of where pixel is jumping across the string
                 float2 track; 
 
-                for (float i = 0; i < 255; i++) {
+                // mandelbrot fractal algorithm 
+                for (float i=0; i < 255; i++) {
                     // update track value based on previous track value
                     track = float2(track.x*track.x-track.y*track.y, 2*track.x * track.y ) + start ; 
 
