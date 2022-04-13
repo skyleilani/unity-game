8aba8a01a0e890e1cedc9151cfd22718c6e264e3
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-04-13 10:03:53 -0400

    Update SerialCommunicationSketch.cpp

--- Arduino/SerialCommunicationSketch.cpp
+++ Arduino/SerialCommunicationSketch.cpp
@@ -1,25 +1,24 @@
-const int pin6_Button = 6; 
-const int pin7_Button = 7; 
-
 void setup(){ 
   Serial.begin(9600);  // set baud rate 
 
-  pinMode(pin6_Button, INPUT);
-  pinMode(pin7_Button, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
 
-  digitalWrite(pin6_Button, HIGH);
-  digitalWrite(pin7_Button, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
 }
 
 void loop() { 
-  if(digitalRead(pin6_Button) == LOW) {    
-    Serial.write(2); // binary instead of ASCII
+  if(digitalRead(6) == LOW) {    
+    Serial.write(2); 
     Serial.flush(); 
     delay(10);
   } 
-  else if (digitalRead(pin7_Button) == LOW) { 
+  else if (digitalRead(7) == LOW){ 
     Serial.write(1);
     Serial.flush();
     delay(10);
   }
+ 
+  
 }
